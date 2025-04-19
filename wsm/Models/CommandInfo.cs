3263c93c4cc0cf82fb5f7ed8dfd07ad017f95fe9
Konstantin Zhigaylo <zero@kostyazero.com>
2025-04-19 21:49:29 +0300

    chore(models): remove unused models

--- wsm/Models/CommandInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace wsm.Models;
-
-public class CommandInfo
-{
-    public string Command { get; set; } = String.Empty;
-    public string Description { get; set; } = String.Empty;
-}
--- wsm/Models/OptionInfo.cs
+++
@@ -1,8 +0,0 @@
-namespace wsm.Models;
-
-public class OptionInfo 
-{
-    public string OptionShort { get; set; } = string.Empty;
-    public string OptionLong { get; set; } = string.Empty;
-    public string OptionDescription { get; set; } = string.Empty;
-}
